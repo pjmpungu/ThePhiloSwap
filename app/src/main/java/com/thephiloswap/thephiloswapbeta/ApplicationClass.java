14ce9b07e949c9b8a80df83349d3d54737eafd8f
Jonathan <pjmpungu@gmail.com>
2021-08-22 16:30:18 -0400

    made some basic edits to application class gradle and manifest files preparing to use backednless

--- app/src/main/java/com/thephiloswap/thephiloswapbeta/ApplicationClass.java
+++ app/src/main/java/com/thephiloswap/thephiloswapbeta/ApplicationClass.java
@@ -2,5 +2,22 @@ package com.thephiloswap.thephiloswapbeta;
 
 import android.app.Application;
 
+import com.backendless.Backendless;
+
 public class ApplicationClass extends Application {
+
+    public static final String APPLICATION_ID = "1E1F1846-2227-DC19-FFEB-E0954203DF00";
+    public static final String API_KEY = "B4C42F06-F43D-4CC0-8FDB-D359417FFD98";
+    public static final String SERVER_URL = "https://api.backendless.com";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Backendless.setUrl(SERVER_URL );
+        Backendless.initApp(getApplicationContext(),
+                APPLICATION_ID,
+                API_KEY );
+
+    }
 }
